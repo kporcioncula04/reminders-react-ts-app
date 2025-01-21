2f1191e3bf38868ed3f86862335bd289f0b6393d
karolina porcioncula <59806218+kporcioncula04@users.noreply.github.com>
2025-01-21 16:14:20 -0500

    call backend using axios and implement useeffect hook

---
+++ src/components/ReminderList.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Reminder from "../models/reminder";
+
+interface ReminderListProps {
+  items: Reminder[];
+}
+
+function ReminderList({ items }: ReminderListProps) {
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li className="list-group-item" key={item.id}>
+          {item.title}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ReminderList;
